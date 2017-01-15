73812970d6673af39a8c9e7e696cf4cf9cef56bb
minchao <minchao.220@gmail.com>
2017-01-15 23:48:16 +0800

    Fix concurrent problem of store.Message

--- smsender/worker.go
+++ smsender/worker.go
@@ -31,17 +31,17 @@ func (w worker) process(message *model.Message) {
 		broker = w.sender.GetBroker(DefaultBroker)
 	}
 
-	logger := log.WithFields(log.Fields{
+	log1 := log.WithFields(log.Fields{
 		"message_id": message.Id,
 		"worker_id":  w.id,
 		"broker":     broker.Name(),
 	})
-	logger.WithField("message", *message).Info("worker process")
+	log1.WithField("message", *message).Info("worker process")
 
 	result = model.NewResult(*message, broker.Name())
 
 	// Save the send record to db
-	rchan1 := w.sender.store.Message().Save(result)
+	rch := w.sender.store.Message().Save(result)
 
 	broker.Send(message, result)
 
@@ -50,24 +50,22 @@ func (w worker) process(message *model.Message) {
 	result.SentTime = &sentTime
 	result.Latency = &latency
 
-	rchan2 := w.sender.store.Message().Update(result)
-
-	logger2 := logger.WithField("result", *result)
+	log2 := log1.WithField("result", *result)
 	switch result.Status {
 	case model.StatusFailed.String():
-		logger2.Error("broker send message failed")
+		log2.Error("broker send message failed")
 	default:
-		logger2.Info("broker send message")
+		log2.Info("broker send message")
 	}
 
 	if message.Result != nil {
 		message.Result <- *result
 	}
 
-	if r := <-rchan1; r.Err != nil {
-		logger.Errorf("store save error: %v", r.Err)
+	if r := <-rch; r.Err != nil {
+		log1.Errorf("store save error: %v", r.Err)
 	}
-	if r := <-rchan2; r.Err != nil {
-		logger.Errorf("store update error: %v", r.Err)
+	if r := <-w.sender.store.Message().Update(result); r.Err != nil {
+		log1.Errorf("store update error: %v", r.Err)
 	}
 }
