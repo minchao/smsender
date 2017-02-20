d638b5c588c8b7d8616e98178d59f3169413f798
minchao <minchao.220@gmail.com>
2017-02-20 13:09:06 +0800

    Load all plugins on app startup

--- main.go
+++ main.go
@@ -8,7 +8,10 @@ import (
 	log "github.com/Sirupsen/logrus"
 	"github.com/minchao/smsender/smsender"
 	"github.com/minchao/smsender/smsender/api"
+	"github.com/minchao/smsender/smsender/providers/aws"
 	"github.com/minchao/smsender/smsender/providers/dummy"
+	"github.com/minchao/smsender/smsender/providers/nexmo"
+	"github.com/minchao/smsender/smsender/providers/twilio"
 	"github.com/minchao/smsender/smsender/web"
 	config "github.com/spf13/viper"
 )
@@ -51,9 +54,35 @@ func main() {
 
 	sender := smsender.SMSender()
 
-	provider := dummy.NewProvider("dummy")
+	dummyProvider := dummy.NewProvider("dummy")
+	sender.Router.AddProvider(dummyProvider)
 
-	sender.Router.AddProvider(provider)
+	if ok := config.IsSet("providers.aws"); ok {
+		provider := aws.Config{
+			Region: config.GetString("providers.aws.region"),
+			ID:     config.GetString("providers.aws.id"),
+			Secret: config.GetString("providers.aws.secret"),
+		}.NewProvider("aws")
+		sender.Router.AddProvider(provider)
+	}
+	if ok := config.IsSet("providers.nexmo"); ok {
+		provider := nexmo.Config{
+			Key:           config.GetString("providers.nexmo.key"),
+			Secret:        config.GetString("providers.nexmo.secret"),
+			EnableWebhook: config.GetBool("providers.nexmo.webhook.enable"),
+		}.NewProvider("nexmo")
+		sender.Router.AddProvider(provider)
+	}
+	if ok := config.IsSet("providers.twilio"); ok {
+		provider := twilio.Config{
+			Sid:           config.GetString("providers.twilio.sid"),
+			Token:         config.GetString("providers.twilio.token"),
+			EnableWebhook: config.GetBool("providers.twilio.webhook.enable"),
+			SiteURL:       config.GetString("http.siteURL"),
+		}.NewProvider("twilio")
+		sender.Router.AddProvider(provider)
+	}
+
 	sender.Router.LoadFromDB()
 
 	api.InitAPI(sender)
