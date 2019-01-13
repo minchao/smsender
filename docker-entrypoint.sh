8b6799d08e120852081e9791444f418c40cd3ece
minchao <minchao.220@gmail.com>
2019-01-13 22:50:17 +0800

    Improve docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
 
 config=/smsender/config/config.yml
+
 MYSQL_USER=${MYSQL_USER:-smsender_user}
 MYSQL_PASSWORD=${MYSQL_PASSWORD:-smsender_password}
 MYSQL_HOST=${MYSQL_HOST:-db}
@@ -8,27 +13,26 @@ MYSQL_PORT=${MYSQL_PORT:-3306}
 MYSQL_DATABASE=${MYSQL_DATABASE:-smsender}
 
 echo "Configure config.yml"
-if [ ! -f ${config} ]
-then
+if [[ ! -f ${config} ]]; then
   cp /config.default.yml ${config}
-  sed -Ei "s/MYSQL_USER/$MYSQL_USER/" ${config}
-  sed -Ei "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/" ${config}
-  sed -Ei "s/MYSQL_HOST/$MYSQL_HOST/" ${config}
-  sed -Ei "s/MYSQL_PORT/$MYSQL_PORT/" ${config}
-  sed -Ei "s/MYSQL_DATABASE/$MYSQL_DATABASE/" ${config}
+  sed -Ei "s/MYSQL_USER/${MYSQL_USER}/" ${config}
+  sed -Ei "s/MYSQL_PASSWORD/${MYSQL_PASSWORD}/" ${config}
+  sed -Ei "s/MYSQL_HOST/${MYSQL_HOST}/" ${config}
+  sed -Ei "s/MYSQL_PORT/${MYSQL_PORT}/" ${config}
+  sed -Ei "s/MYSQL_DATABASE/${MYSQL_DATABASE}/" ${config}
   echo OK
 else
   echo SKIP
 fi
 
-echo "Wait until database $MYSQL_HOST:$MYSQL_PORT is ready..."
-until nc -z ${MYSQL_HOST} ${MYSQL_PORT}
+echo "Wait until database ${MYSQL_HOST}:${MYSQL_PORT} is ready..."
+until nc -z "${MYSQL_HOST}" "${MYSQL_PORT}"
 do
-    sleep 1
+  sleep 1
 done
 
 sleep 1
 
 echo "Starting app"
-cd /smsender
-./smsender -c ${config}
+cd /smsender || exit 255
+./smsender -c "${config}"
